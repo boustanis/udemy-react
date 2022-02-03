ea9b30c561348654471029d3304616bfd5b95313
walkman33 <boustanis@gmail.com>
2022-02-03 15:34:29 +0200

    added variables in ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(props){
+function ExpenseItem(){
+    let expenseAmount = 200.00;
+    let expenseTitle = 'Car Insurance';
+
     return(
-        <div>
-            <p>Hello I am an expense item!</p>
+        <div className="expense-item">
+            <div>3rd February 2022</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div>
         </div>
     );
 }
