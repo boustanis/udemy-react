649f59baac285dc3d9815f2eb5f41f0edd58431d
walkman33 <boustanis@gmail.com>
2022-02-03 15:02:35 +0200

    added components folder and created the first custom component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props){
+    return(
+        <div>
+            <p>Hello I am an expense item!</p>
+        </div>
+    );
+}
+
+export default ExpenseItem;
