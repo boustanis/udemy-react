0bfc25945ae5b5fd88e6e9777bdc5194afa4c5a2
walkman33 <boustanis@gmail.com>
2022-02-05 01:11:02 +0200

    NewExpense folder with NewExpense component added for user input

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,8 +4,8 @@ import './Expenses.css';
 
 function Expenses(props){
     // make expenses array with <ExpenseItem> elements
-const expenses = props.expenses.map((expense)=>{
-    return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>
+const expenses = props.expenses.map((expense,index)=>{
+    return <ExpenseItem key={index} title={expense.title} amount={expense.amount} date={expense.date}/>
 })
     return(
         <Card className="expenses">
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import './NewExpense.css';
+
+const newExpense = (props) => {
+    return(
+        <div>
+            <form></form>
+        </div>
+    )
+};
+
+export default newExpense;
