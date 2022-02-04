c862161a4bb9ceef34a22db1c3e923990cf3d37b
walkman33 <boustanis@gmail.com>
2022-02-04 14:19:55 +0200

    Expenses component ready for merge with master

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,8 +3,8 @@ import './Expenses.css';
 
 function Expenses(props){
     // make expenses array with <ExpenseItem> elements
-const expenses = props.expenses.map((expense,index)=>{
-    return <ExpenseItem key={index} title={expense.title} amount={expense.amount} date={expense.date}/>
+const expenses = props.expenses.map((expense)=>{
+    return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>
 })
     return(
         <div className="expenses">
