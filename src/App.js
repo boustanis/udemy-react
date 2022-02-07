234e585f3e9a09d3309b431107dbe6c6a314387c
walkman33 <boustanis@gmail.com>
2022-02-07 22:04:07 +0200

    child to parent component communication added

--- src/App.js
+++ src/App.js
@@ -17,9 +17,14 @@ const expenses = [
 ]
 
 function App() {
+  const addExpenseHandler = expense => {
+    console.log('In App.js => ');
+    console.log(expense);
+  };
+
   return(
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses expenses={expenses}/>
    </div>
   );
